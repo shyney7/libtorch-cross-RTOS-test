8025d615a78dc66ba86accfcbf93b02e6fb5f1c2
MarcelUbuntucross <ligimixi@gmail.com>
2020-05-12 01:26:15 -0700

    included torch/torch.h

---
+++ build/Buildfiles_with_-stdlib=libstdc++/CMakeFiles/3.10.2/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-5.3.0-51-generic")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "5.3.0-51-generic")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/marcel/Documents/libtorch_cross_test/cmake/qnx_7.0.0_linux_x86_64.cmake")
+
+set(CMAKE_SYSTEM "QNX")
+set(CMAKE_SYSTEM_NAME "QNX")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "x86_64")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
